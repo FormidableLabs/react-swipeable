214c40ec41a99ac158004d3d3be151ded9042a0d
brian emil hartz <bhartz@shutterstock.com>
2018-06-14 16:35:10 -0700

    Updates

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App version={SWIPEABLE_VERSION} />, document.getElementById('app'));
