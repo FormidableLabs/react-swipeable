913727f4e6fdb93eb2c0321b70f7770549328459
Brian Emil Hartz <brianhartz@gmail.com>
2019-01-28 21:03:01 -0700

    update examples with new v5 codesandbox carousel

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ React swipe and touch event handler component & hook
 
 [![build status](https://img.shields.io/travis/dogfessional/react-swipeable/master.svg?style=flat-square)](https://travis-ci.org/dogfessional/react-swipeable) [![npm version](https://img.shields.io/npm/v/react-swipeable.svg?style=flat-square)](https://www.npmjs.com/package/react-swipeable) [![npm downloads](https://img.shields.io/npm/dm/react-swipeable.svg?style=flat-square)](https://www.npmjs.com/package/react-swipeable) [![gzip size](https://flat.badgen.net/bundlephobia/minzip/react-swipeable)](https://bundlephobia.com/result?p=react-swipeable)
 
-[![Edit qq7759m3lq](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/qq7759m3lq?module=%2Fsrc%2FCarousel.js)
+[![Edit react-swipeable image carousel with hook](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/lrk6955l79?module=%2Fsrc%2FCarousel.js)
 
 [Github Pages Demo](https://dogfessional.github.io/react-swipeable/)
 
@@ -18,7 +18,7 @@ The component is still included and migration to v5 is straightforward. Please s
 
 ### Installation
 ```
-$ npm install --save react-swipeable
+$ npm install react-swipeable
 ```
 
 ### Api
--- examples/app/App.js
+++ examples/app/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ScrollUp from 'react-scroll-up';
 import FeatureTestConsole from './FeatureTestConsole.js';
-import SimpleCarousel from './SimpleCarousel.js';
+
+const scrollUpArrowStyles = { fontSize: '20px', border: '2px solid black', borderRadius: '50%', padding: '10px', background: 'white' };
 
 export default function App({version}) {
   return (
     <div className="row">
+      <ScrollUp showUnder={20}
+                easing={'easeOutCubic'}
+                duration={500}>
+          <span style={scrollUpArrowStyles}>▲</span>
+      </ScrollUp>
       <div className="medium-6 column" style={{width: "95%"}}>
         <h1>react-swipeable&nbsp;<a href="https://github.com/dogfessional/react-swipeable" style={{fontSize: "0.75rem"}}>v{version}</a></h1>
         <dl>
@@ -15,7 +22,9 @@ export default function App({version}) {
         <hr />
         <FeatureTestConsole />
         <hr />
-        <SimpleCarousel />
+        <div className="row" id="SimpleCarousel">
+          <iframe src="https://codesandbox.io/embed/lrk6955l79" style={{width:'100%', height:'500px', border:'0', borderRadius:'4px', overflow:'hidden'}} sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin" />
+        </div>
         <hr />
         <p style={{"marginBottom": "2rem"}}>
           Thanks for checking out the example app! Let us know if you find any bugs, and&nbsp;
