65d19f15eb17d4564e833b4bb3a432babb6f174e
Adam Bowles <github@adambowl.es>
2019-03-25 14:14:38 +0000

    Refactor index.d.ts
    Rewrote the type definition to actually export a react component

--- index.d.ts
+++ index.d.ts
@@ -1,46 +1,42 @@
-import * as React from 'react'
+declare module 'react-swipeable' {
+  import * as React from 'react'
 
-declare class ReactSwipeable<T extends Element = HTMLElement> extends React.Component<
-  ReactSwipeable.SwipeableProps<T>
-> {}
-
-interface EventData<T> {
-  event: React.TouchEvent<T>;
-  deltaX: number;
-  deltaY: number;
-  absX: number;
-  absY: number;
-  velocity: number;
-  dir: 'Left' | 'Right' | 'Up' | 'Down';
-}
-
-declare namespace ReactSwipeable {
-  type Callback<T extends Element = HTMLElement> = ({}: EventData<T>) => void
-  type DirectionCallback<T extends Element = HTMLElement> = ({}: EventData<T>) => void
-
-  interface SwipeableProps<T extends Element = HTMLElement>
-    extends React.ClassAttributes<ReactSwipeable<T>>,
-      React.HTMLAttributes<T> {
-    // Event data
-    onSwiped?: Callback<T>
-    onSwipedLeft?: DirectionCallback<T>
-    onSwipedRight?: DirectionCallback<T>
-    onSwipedUp?: DirectionCallback<T>
-    onSwipedDown?: DirectionCallback<T>
-    onSwiping?: Callback<T>
-
-    // Configuration Props
-    delta?: number
-    preventDefaultTouchmoveEvent?: boolean
-    trackTouch?: boolean
-    trackMouse?: boolean
-    rotationAngle?: number
-
-    // Component Specific Props
-    nodeName?: string
-    innerRef?: React.Ref<T>
-    children?: React.ReactNode
+  interface EventData<T> {
+    event: React.TouchEvent<T>
+    deltaX: number
+    deltaY: number
+    absX: number
+    absY: number
+    velocity: number
+    dir: 'Left' | 'Right' | 'Up' | 'Down'
   }
-}
 
-export = ReactSwipeable
+  namespace ReactSwipeable {
+    type Callback<T extends Element = HTMLElement> = ({}: EventData<T>) => void
+    type DirectionCallback<T extends Element = HTMLElement> = ({}: EventData<T>) => void
+
+    interface SwipeableProps<T extends Element = HTMLElement> extends React.ClassAttributes<Swipeable<T>>, React.HTMLAttributes<T> {
+      // Event data
+      onSwiped?: Callback<T>
+      onSwipedLeft?: DirectionCallback<T>
+      onSwipedRight?: DirectionCallback<T>
+      onSwipedUp?: DirectionCallback<T>
+      onSwipedDown?: DirectionCallback<T>
+      onSwiping?: Callback<T>
+
+      // Configuration Props
+      delta?: number
+      preventDefaultTouchmoveEvent?: boolean
+      trackTouch?: boolean
+      trackMouse?: boolean
+      rotationAngle?: number
+
+      // Component Specific Props
+      nodeName?: string
+      innerRef?: React.Ref<T>
+      children?: React.ReactNode
+    }
+  }
+
+  export class Swipeable<T extends Element = HTMLElement> extends React.Component<ReactSwipeable.SwipeableProps<T>> {}
+}
