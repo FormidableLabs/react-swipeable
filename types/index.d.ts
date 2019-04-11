54f0fb25d30506fae5a92d72ed8aead5b618f580
Adam Bowles <github@adambowl.es>
2019-04-11 04:02:43 +0100

    Add TypeScript types for Swipeable Component v5 (#133)
    * Add TypeScript types for v5
    
    * Add dtslint to npm test script

---
+++ types/index.d.ts
@@ -0,0 +1,43 @@
+// TypeScript Version: 3.0
+
+import * as React from 'react'
+
+export interface EventData {
+  event: MouseEvent | TouchEvent
+  deltaX: number
+  deltaY: number
+  absX: number
+  absY: number
+  velocity: number
+  dir: 'Left' | 'Right' | 'Up' | 'Down'
+}
+
+export type SwipeCallback = ({}: EventData) => void
+
+export interface SwipeableOptions {
+  // Event handler/callbacks
+  onSwiped?: SwipeCallback
+  onSwipedLeft?: SwipeCallback
+  onSwipedRight?: SwipeCallback
+  onSwipedUp?: SwipeCallback
+  onSwipedDown?: SwipeCallback
+  onSwiping?: SwipeCallback
+
+  // Configuration Props
+  delta?: number
+  preventDefaultTouchmoveEvent?: boolean
+  trackTouch?: boolean
+  trackMouse?: boolean
+  rotationAngle?: number
+}
+
+// Component Specific Props
+export interface SwipeableProps {
+  nodeName?: string
+  innerRef?: ({}: HTMLElement) => void
+  children?: React.ReactNode
+  style?: React.CSSProperties
+  className?: string
+}
+
+export class Swipeable extends React.PureComponent<SwipeableProps & SwipeableOptions> {}
---
+++ types/test.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { Swipeable, SwipeableProps, SwipeCallback } from 'react-swipeable'
+
+class SampleComponent extends React.PureComponent<SwipeableProps> {
+  private readonly handleSwiped: SwipeCallback = () => {}
+  private readonly handleSwipedLeft: SwipeCallback = () => {}
+  private readonly handleSwipedRight: SwipeCallback = () => {}
+  private readonly handleSwipedUp: SwipeCallback = () => {}
+  private readonly handleSwipedDown: SwipeCallback = () => {}
+  private readonly handleSwiping: SwipeCallback = () => {}
+
+  render() {
+    return (
+      <Swipeable
+        // Event handler/callbacks
+        onSwiped={this.handleSwiped}
+        onSwipedLeft={this.handleSwipedLeft}
+        onSwipedRight={this.handleSwipedRight}
+        onSwipedUp={this.handleSwipedUp}
+        onSwipedDown={this.handleSwipedDown}
+        onSwiping={this.handleSwiping}
+
+        // Configuration Props
+        delta={10}
+        preventDefaultTouchmoveEvent={true}
+        trackTouch={true}
+        trackMouse={true}
+        rotationAngle={10}
+
+        // Component Specific Props
+        nodeName="div"
+        innerRef={() => {}}
+        style={{ backgroundColor: 'blue' }}
+        className="classname"
+      >
+        <div>This element can be swiped</div>
+      </Swipeable>
+    )
+  }
+}
+
+class SwipeableDiv extends Swipeable {}
+
+const TestComponent: React.StatelessComponent = _ => {
+  const handleSwiped: SwipeCallback = () => {}
+
+  return (
+    <SwipeableDiv nodeName="div" onSwiped={handleSwiped}>
+      <div>this is sample code.</div>
+    </SwipeableDiv>
+  )
+}
