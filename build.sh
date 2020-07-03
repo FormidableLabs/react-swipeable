dbbde7c485a4ad283f4bd0e5839c6057dd205590
kraftwerk28 <kefirchik3@gmail.com>
2020-07-03 13:36:57 +0300

    fix: build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 if [ -f is-installed ]; then
+  echo "Skipping installation hook."
+  rm -f is-installed
+else
   echo > is-installed
   npm install
   npm run build
   rm -rf node_modules examples rollup.config.js
-else
-  echo "Skipping installation hook."
-  rm -f is-installed
 fi
