29d03dfa55a7e45f06af0ec69b59e381874365e8
Stefan Huynh <stefan.huynh@formidable.com>
2021-05-13 14:05:23 -0700

    Migrate examples (#238)
    * Make examples self-contained for codesandbox
    
    Co-authored-by: Emil Hartz <emil.hartz@formidable.com>

---
+++ examples/app/SimpleCarousel/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Item } from '../components';
+import Carousel from './Carousel';
+// Carousel originally copied from:
+// https://medium.com/@incubation.ff/build-your-own-css-carousel-in-react-part-one-86f71f6670ca
+
+function SimpleCarousel() {
+  return (
+    <div>
+      <h5 style={{ marginBottom: '20px' }}>
+        <strong>🖼 Image Carousel</strong>
+      </h5>
+
+      <Carousel>
+        <Item img="https://unsplash.it/475/205" />
+        <Item img="https://unsplash.it/476/205" />
+        <Item img="https://unsplash.it/477/205" />
+        <Item img="https://unsplash.it/478/205" />
+        <Item img="https://unsplash.it/479/205" />
+      </Carousel>
+    </div>
+  );
+}
+
+export default SimpleCarousel;
