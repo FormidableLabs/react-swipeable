4fa52584feeda970058ba9af38a52ebd50e31b83
Sawyer Burnett <sawyer.burnett@formidable.com>
2024-03-21 10:05:14 -0700

    initial add of docs typescript site

---
+++ docs/docs/intro.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro
+
+Maybe we should have a react-swipeable tutorial?
+
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,111 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'React Swipeable',
+  tagline: 'Documentation Site',
+  favicon: 'img/favicon.ico', //todo: update
+
+  // Set the production url of your site here
+  url: 'https://commerce.nearform.com/',
+
+  // Set the /<baseUrl>/ pathname under which your site is served
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/FormidableLabs/react-swipeable/tree/main/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Todo',
+              href: 'https://commerce.nearform.com',
+            },
+            {
+              label: 'Todo',
+              href: 'https://commerce.nearform.com',
+            },
+            {
+              label: 'Todo',
+              href: 'https://commerce.nearform.com',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/FormidableLabs/react-swipeable',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Nearform_Commerce`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
