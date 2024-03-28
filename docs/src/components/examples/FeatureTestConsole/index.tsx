7a075c587e6f8701dba383e5c89215ad737ff84b
Sawyer Burnett <sawyer.burnett@formidable.com>
2024-03-28 14:21:42 -0700

    update example to look better in dark mode

--- docs/src/components/examples/FeatureTestConsole/index.tsx
+++ docs/src/components/examples/FeatureTestConsole/index.tsx
@@ -1,19 +1,20 @@
-import React, {Component} from 'react';
-import { RowSimpleCheckbox } from './TableComponents';
-import SwipeableHook from './SwipeableHook';
+import React, { Component } from "react";
+import { RowSimpleCheckbox } from "./TableComponents";
+import SwipeableHook from "./SwipeableHook";
+import { PatternBox } from "../components";
 
-const DIRECTIONS = ['Left', 'Right', 'Up', 'Down'];
+const DIRECTIONS = ["Left", "Right", "Up", "Down"];
 
 const initialState = {
   swiping: false,
   swiped: false,
   tapped: false,
-  swipingDirection: '',
-  swipedDirection: '',
+  swipingDirection: "",
+  swipedDirection: "",
 };
-const INFINITY = 'Infinity';
+const INFINITY = "Infinity";
 const initialStateSwipeable = {
-  delta: '10',
+  delta: "10",
   preventScrollOnSwipe: false,
   trackMouse: false,
   trackTouch: true,
@@ -50,20 +51,32 @@ interface IState {
 export default class Main extends Component<any, IState> {
   constructor(props: any) {
     super(props);
-    this.state = Object.assign({}, initialState, initialStateSwipeable, initialStateApplied);
+    this.state = Object.assign(
+      {},
+      initialState,
+      initialStateSwipeable,
+      initialStateApplied
+    );
     this.updateValue = this.updateValue.bind(this);
   }
 
   resetState(resetAll?: boolean) {
     if (resetAll) {
-      this.setState(Object.assign({}, initialState, initialStateSwipeable, initialStateApplied));
+      this.setState(
+        Object.assign(
+          {},
+          initialState,
+          initialStateSwipeable,
+          initialStateApplied
+        )
+      );
     } else {
       this.setState(initialState);
     }
   }
 
   onSwiped(args: any) {
-    console.log('swiped args: ', args)
+    console.log("swiped args: ", args);
     this.setState({
       swiped: true,
       swiping: false,
@@ -71,7 +84,7 @@ export default class Main extends Component<any, IState> {
   }
 
   onSwiping(args: any) {
-    console.log('swiping args: ', args)
+    console.log("swiping args: ", args);
 
     this.setState({
       swiping: true,
@@ -81,13 +94,13 @@ export default class Main extends Component<any, IState> {
   }
 
   onTap(args: any) {
-    console.log('tap args: ', args)
+    console.log("tap args: ", args);
 
     this.setState({
       swiping: false,
       swiped: false,
-      tapped: true
-    })
+      tapped: true,
+    });
   }
 
   onSwipedDirection(direction: any) {
@@ -98,23 +111,28 @@ export default class Main extends Component<any, IState> {
 
   updateValue(type: string, value: any) {
     // @ts-ignore
-    this.setState({ [type]: value, });
+    this.setState({ [type]: value });
   }
 
   _renderAppliedDirRow(dir: string) {
     // @ts-ignore
     const checked = this.state[`onSwiped${dir}Applied`];
-    // @ts-ignore
-    const cssJs = {color: this.state[`onSwiped${dir}Applied`] ? '#000000' : '#cccccc'}
+
     return (
       <tr key={`appliedDirRow${dir}`}>
         <td className="text-center">
-          <input type="checkbox" style={{margin: "0"}} checked={checked}
-            onChange={(e)=>this.updateValue(`onSwiped${dir}Applied`, e.target.checked)} />
+          <input
+            type="checkbox"
+            style={{ margin: "0" }}
+            checked={checked}
+            onChange={(e) =>
+              this.updateValue(`onSwiped${dir}Applied`, e.target.checked)
+            }
+          />
         </td>
-        <td style={cssJs}>{dir}</td>
+        <td>{dir}</td>
       </tr>
-    )
+    );
   }
 
   render() {
@@ -138,30 +156,36 @@ export default class Main extends Component<any, IState> {
     } = this.state;
 
     const isSwipeDurationInfinity = swipeDuration === INFINITY;
-    const swipeDurationTextValue = isSwipeDurationInfinity ? INFINITY : swipeDuration;
-    const isSwipeDurationNumber = isSwipeDurationInfinity ? Infinity : !(isNaN(swipeDuration as any) || swipeDuration === '');
+    const swipeDurationTextValue = isSwipeDurationInfinity
+      ? INFINITY
+      : swipeDuration;
+    const isSwipeDurationNumber = isSwipeDurationInfinity
+      ? Infinity
+      : !(isNaN(swipeDuration as any) || swipeDuration === "");
 
-    const isDeltaNumber = !(isNaN(delta as any) || delta === '');
-    const isRotationAngleNumber = !(isNaN(rotationAngle as any) || rotationAngle === '');
+    const isDeltaNumber = !(isNaN(delta as any) || delta === "");
+    const isRotationAngleNumber = !(
+      isNaN(rotationAngle as any) || rotationAngle === ""
+    );
     const deltaNum = isDeltaNumber ? +delta : 10;
     const rotationAngleNum = isRotationAngleNumber ? +rotationAngle : 0;
 
     const swipeableStyle = {
-      fontSize: '0.75rem',
-      touchAction: stopScrollCss ? 'none' : 'auto',
+      fontSize: "0.75rem",
+      touchAction: stopScrollCss ? "none" : "auto",
     };
 
     const boundSwipes = getBoundSwipes(this);
     let swipeableDirProps: any = {};
     if (onSwipingApplied) {
       // @ts-ignore
-      swipeableDirProps.onSwiping = (...args: any)=>this.onSwiping(...args);
+      swipeableDirProps.onSwiping = (...args: any) => this.onSwiping(...args);
     }
     if (onSwipedApplied) {
       // @ts-ignore
-      swipeableDirProps.onSwiped = (...args: any)=>this.onSwiped(...args);
+      swipeableDirProps.onSwiped = (...args: any) => this.onSwiped(...args);
     }
-    if(onTapApplied) {
+    if (onTapApplied) {
       // @ts-ignore
       swipeableDirProps.onTap = (...args: any) => this.onTap(...args);
     }
@@ -169,7 +193,6 @@ export default class Main extends Component<any, IState> {
     return (
       <div className="row">
         <div className="small-12 column">
-          <h5><strong>💻 Test react-swipeable features.</strong></h5>
           <SwipeableHook
             {...boundSwipes}
             {...swipeableDirProps}
@@ -180,89 +203,178 @@ export default class Main extends Component<any, IState> {
             rotationAngle={rotationAngleNum}
             swipeDuration={swipeDuration}
             className="callout hookComponent"
-            style={swipeableStyle}>
-              <div onTouchStart={()=>this.resetState()} onMouseDown={()=>this.resetState()}>
-                <h5>Swipe inside here to test</h5>
-                <p>See output below and check the console for 'onSwiping' and 'onSwiped' callback output(open dev tools)</p>
-                <span>You can also 'toggle' the swiped props being applied to this container below.</span>
-              </div>
+            style={swipeableStyle}
+          >
+            <div
+              onTouchStart={() => this.resetState()}
+              onMouseDown={() => this.resetState()}
+            >
+              <PatternBox className="card self-center text-center">
+                <div className="card__header">
+                  <h3>👆 Swipeable Box 👆</h3>
+                </div>
+                <div className="card__body">
+                  Swipe within this box to test the <code>useSwipeable</code>{" "}
+                  hook. Open the browser console window to see the event
+                  details.
+                </div>
+              </PatternBox>
+            </div>
           </SwipeableHook>
+          <p>
+            See output below and check the console for 'onSwiping' and
+            'onSwiped' callback output(open dev tools)
+          </p>
+          <span>
+            You can also 'toggle' the swiped props being applied to this
+            container below.
+          </span>
           <table>
             <thead>
-              <tr><th>Applied?</th><th>Action</th><th>Output</th></tr>
+              <tr>
+                <th>Applied?</th>
+                <th>Action</th>
+                <th>Output</th>
+              </tr>
             </thead>
             <tbody>
-              <tr style={{color: onSwipingApplied ? '#000000' : '#cccccc'}}>
+              <tr>
                 <td className="text-center">
-                  <input type="checkbox" checked={onSwipingApplied} style={{margin: "0"}}
-                    onChange={(e)=>this.updateValue('onSwipingApplied', e.target.checked)} />
+                  <input
+                    type="checkbox"
+                    checked={onSwipingApplied}
+                    style={{ margin: "0" }}
+                    onChange={(e) =>
+                      this.updateValue("onSwipingApplied", e.target.checked)
+                    }
+                  />
                 </td>
-                <td>onSwiping</td><td>{swiping ? 'True' : 'False'}</td>
+                <td>onSwiping</td>
+                <td>{swiping ? "True" : "False"}</td>
               </tr>
-              <tr style={{color: onSwipedApplied ? '#000000' : '#cccccc'}}>
+              <tr>
                 <td className="text-center">
-                  <input type="checkbox" checked={onSwipedApplied} style={{margin: "0"}}
-                    onChange={(e)=>this.updateValue('onSwipedApplied', e.target.checked)} />
+                  <input
+                    type="checkbox"
+                    checked={onSwipedApplied}
+                    style={{ margin: "0" }}
+                    onChange={(e) =>
+                      this.updateValue("onSwipedApplied", e.target.checked)
+                    }
+                  />
                 </td>
-                <td>onSwiped</td><td>{swiped ? 'True' : 'False'}</td>
+                <td>onSwiped</td>
+                <td>{swiped ? "True" : "False"}</td>
               </tr>
-                            <tr style={{color: onTapApplied ? '#000000' : '#cccccc'}}>
+              <tr>
                 <td className="text-center">
-                  <input type="checkbox" checked={onTapApplied} style={{margin: "0"}}
-                    onChange={(e)=>this.updateValue('onTapApplied', e.target.checked)} />
+                  <input
+                    type="checkbox"
+                    checked={onTapApplied}
+                    style={{ margin: "0" }}
+                    onChange={(e) =>
+                      this.updateValue("onTapApplied", e.target.checked)
+                    }
+                  />
                 </td>
-                <td>onTap</td><td>{tapped ? 'True' : 'False'}</td>
+                <td>onTap</td>
+                <td>{tapped ? "True" : "False"}</td>
               </tr>
               <tr>
                 <td className="text-center"></td>
-                <td>onSwiping Direction</td><td>{swipingDirection}</td>
+                <td>onSwiping Direction</td>
+                <td>{swipingDirection}</td>
               </tr>
               <tr>
                 <td className="text-center">
-                  <a href="#" onClick={(e)=>(e.preventDefault(),this.updateValue('showOnSwipeds', !showOnSwipeds))}>
-                    {showOnSwipeds ? '↑ Hide ↑' : '↓ Show ↓'}
+                  <a
+                    href="#"
+                    onClick={(e) => (
+                      e.preventDefault(),
+                      this.updateValue("showOnSwipeds", !showOnSwipeds)
+                    )}
+                  >
+                    {showOnSwipeds ? "↑ Hide ↑" : "↓ Show ↓"}
                   </a>
                 </td>
-                <td>onSwiped Direction</td><td>{swipedDirection}</td>
+                <td>onSwiped Direction</td>
+                <td>{swipedDirection}</td>
               </tr>
-              {showOnSwipeds && <tr>
-                <td className="text-center" colSpan={3}>
-                  <table id="appliedDirs">
-                    <thead>
-                      <tr><th colSpan={2} className="text-center">onSwiped</th></tr>
-                      <tr><th>Applied?</th><th>Direction</th></tr>
-                    </thead>
-                    <tbody>
-                      {DIRECTIONS.map(this._renderAppliedDirRow.bind(this))}
-                    </tbody>
-                  </table>
-                </td>
-              </tr>}
+              {showOnSwipeds && (
+                <tr>
+                  <td className="text-center" colSpan={3}>
+                    <table id="appliedDirs">
+                      <thead>
+                        <tr>
+                          <th colSpan={2} className="text-center">
+                            onSwiped
+                          </th>
+                        </tr>
+                        <tr>
+                          <th>Applied?</th>
+                          <th>Direction</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {DIRECTIONS.map(this._renderAppliedDirRow.bind(this))}
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              )}
               <tr>
-                <td colSpan={2} className="text-center">delta:</td>
+                <td colSpan={2} className="text-center">
+                  delta:
+                </td>
                 <td>
-                  <input type="text"
-                    style={{margin: '0px', border: !isDeltaNumber ? '2px solid red' : ''}}
-                    onChange={(e)=>this.updateValue('delta', getVal(e))} value={delta}/>
+                  <input
+                    type="text"
+                    style={{
+                      margin: "0px",
+                      border: !isDeltaNumber ? "2px solid red" : "",
+                    }}
+                    onChange={(e) => this.updateValue("delta", getVal(e))}
+                    value={delta}
+                  />
                 </td>
               </tr>
               <tr>
-                <td colSpan={2} className="text-center">rotationAngle:</td>
+                <td colSpan={2} className="text-center">
+                  rotationAngle:
+                </td>
                 <td>
-                  <input type="text"
-                    style={{margin: '0px', border: !isRotationAngleNumber ? '2px solid red' : ''}}
-                    onChange={(e)=>this.updateValue('rotationAngle', getVal(e))} value={rotationAngle}/>
+                  <input
+                    type="text"
+                    style={{
+                      margin: "0px",
+                      border: !isRotationAngleNumber ? "2px solid red" : "",
+                    }}
+                    onChange={(e) =>
+                      this.updateValue("rotationAngle", getVal(e))
+                    }
+                    value={rotationAngle}
+                  />
                 </td>
               </tr>
               <tr>
                 <td colSpan={2} className="text-center">
                   swipeDuration:
-                  <div><b>(ms | Infinity)</b></div>
+                  <div>
+                    <b>(ms | Infinity)</b>
+                  </div>
                 </td>
                 <td>
-                  <input type="text"
-                    style={{margin: '0px', border: !isSwipeDurationNumber ? '2px solid red' : ''}}
-                    onChange={(e)=>this.updateValue('swipeDuration', getVal(e))} value={swipeDurationTextValue}/>
+                  <input
+                    type="text"
+                    style={{
+                      margin: "0px",
+                      border: !isSwipeDurationNumber ? "2px solid red" : "",
+                    }}
+                    onChange={(e) =>
+                      this.updateValue("swipeDuration", getVal(e))
+                    }
+                    value={swipeDurationTextValue}
+                  />
                 </td>
               </tr>
               <RowSimpleCheckbox
@@ -282,7 +394,7 @@ export default class Main extends Component<any, IState> {
               />
             </tbody>
           </table>
-          <table style={{width: "100%"}}>
+          <table style={{ width: "100%" }}>
             <tbody>
               <RowSimpleCheckbox
                 value={stopScrollCss}
@@ -292,19 +404,28 @@ export default class Main extends Component<any, IState> {
               />
             </tbody>
           </table>
-          <button type="button" className="tiny button expanded" onClick={()=>this.resetState(true)}>Reset All Options</button>
+          <button
+            type="button"
+            className="tiny expanded"
+            onClick={() => this.resetState(true)}
+          >
+            Reset All Options
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
 function getBoundSwipes(component: any) {
   let boundSwipes = {};
-  DIRECTIONS.forEach((dir)=>{
+  DIRECTIONS.forEach((dir) => {
     if (component.state[`onSwiped${dir}Applied`]) {
       // @ts-ignore
-      boundSwipes[`onSwiped${dir}`] = component.onSwipedDirection.bind(component, dir);
+      boundSwipes[`onSwiped${dir}`] = component.onSwipedDirection.bind(
+        component,
+        dir
+      );
     }
   });
   return boundSwipes;
